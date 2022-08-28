fc010e93032705f84c1e1f44f5cbda48797361f9
Drennann <delpan.drennan@gmail.com>
2022-08-28 14:31:47 -0300

    addRedux

--- src/components/Proyect.js
+++ src/components/Proyect.js
@@ -14,7 +14,6 @@ export default function Proyect({end, proyectoImg, proyectoUrlDeploy, proyectoUr
         <div className={end ? "Proyect end" : "Proyect"}>
             <img className="imgproy"alt="img" src={proyectoImg} width="355" height="196"
             onMouseOver={onMouseOverHandler}></img>
-            {isHovering && <p className="hovering">{proyectoDescription}</p>}
         </div>
         </a>
     )
