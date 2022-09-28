7bbdd8c3d2a19db3cd55940fe6a0a0fe4bb49445
Drennann <delpan.drennan@gmail.com>
2022-09-28 05:15:55 -0300

    add 2 proyects

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -5,6 +5,8 @@ export default function Proyects({dataaos}){
     console.log(dataaos)
     return(
         <div className="Proyects" id="#PROYECTSID">
+            <Proyect proyectoDescription = "video game app, you can search different league of legends characters. " proyectoImg={"CoachApp.png"} proyectoUrlDeploy="http://bechallenger.com/" proyectoUrlGithub="https://github.com/Drennann/CoachApp"/>
+            <Proyect end="end" proyectoDescription = "Table that receives crypto prices in real time" proyectoImg={"ChatApp.png"} proyectoUrlDeploy="https://chatapp.up.railway.app/" proyectoUrlGithub="https://github.com/Drennann/ChatApp"/>
             <Proyect proyectoDescription = "video game app, you can search different league of legends characters. " proyectoImg={"leagueapp.png"} proyectoUrlDeploy="https://drennann.github.io/LeagueApp" proyectoUrlGithub="https://github.com/Drennann/LeagueApp"/>
             <Proyect end="end" proyectoDescription = "Online to-do list application, connected to a database and with a backend included" proyectoImg={"todo.png"} proyectoUrlDeploy="https://mern-stack-2-delpan.herokuapp.com/" proyectoUrlGithub="https://dashboard.heroku.com/apps/mern-stack-2-delpan"/>
             <Proyect proyectoDescription = "A simplified chess game. react, javascript, html and css were used" proyectoImg={"chess.png"} proyectoUrlDeploy="https://drennann.github.io/Chess/" proyectoUrlGithub="https://github.com/Drennann/Chess"/>
